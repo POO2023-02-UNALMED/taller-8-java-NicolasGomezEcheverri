81f14ab8919faa4ed81f29e252a24f5befba06d2
Nicolas Gomez Echeverri <141961453+NicolasGomezEcheverri@users.noreply.github.com>
2023-10-16 18:11:32 -0500

    futbolista

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,52 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Object> {
+    
+    private String nombre;
+    private int edad;
+    private final String posicion;
+
+    public Futbolista() {
+        this("Maradona", 30, "delantero");
+    }
+
+    public Futbolista(String nombre, int edad, String posicion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.posicion = posicion;
+    }
+
+    public String toString() {
+        return "El futbolista " + nombre + " tiene " + edad + ", y juega de " + posicion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public boolean equals(Futbolista f) {
+        return this == f;
+    }
+
+    public abstract boolean jugarConLasManos();
+
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
